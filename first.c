78d1a3a33654f8533a1b974d26f5421546dc17fb
Shivamyadavz <shivamkumary9695@gmail.com>
2026-05-28 19:34:15 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,factorial=1;
+    printf("Enter the number : ");
+    scanf("%d",&a);
+    for(int i=1;i<=a;i++){
+        factorial*=i;
+    }
+    printf("The factorial of %d is %d",a,factorial);
+    return 0;
+}
